cd3ab51f43e97f381bfa843bf1067e9d8903551a
KonradCzam <konradcz.sluzbowy@gmail.com>
2018-08-08 20:15:25 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Integer result = add(1,2);
+        System.out.println("Konrad Czmanskis result: " + result.toString());
+    }
+    public static Integer add(Integer a, Integer b){
+        Integer c;
+        c = a+b;
+        c = a+b;
+        c = a+b;
+
+        return c;
+    }
+}

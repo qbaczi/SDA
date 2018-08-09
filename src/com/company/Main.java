62c4dbc3978855f12da43faa9b8d027b10d74152
KonradCzam <konradcz.sluzbowy@gmail.com>
2018-08-09 09:08:20 +0200

    This totally works fine. For sure !

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,13 @@ public class Main {
 
     public static void main(String[] args)
     {
-        Integer result = add(1,2);
+        Integer result = divide(1,2);
         System.out.println("Konrad Czmanskis result: " + result.toString());
     }
-    public static Integer add(Integer a, Integer b){
+    public static Integer divide(Integer a, Integer b){
         Integer c;
-        c = a+b;
-        c = a+b;
-        c = a+b;
+        c=a/b;
 
-        return c;
+        return c + c;
     }
 }
